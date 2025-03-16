9dc308467f8c3894242584310ed51d307a9ebafa
AlejandroTobar19 <20215249@esen.edu.sv>
2025-03-15 19:45:12 -0600

    correccion de error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router"
 import { IndexPage } from "./pages/IndexPage"
-import { FavoritesPage } from "./pages/FAvoritesPage"
+import { FavoritesPage } from "./pages/FavoritesPage"
 import Layout from "./layout/Layout"
 
 const App = () => {
